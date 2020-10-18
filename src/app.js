41b9d4616a1075289bd2bbb5d35c5081875e3bde
Chase Barker <chaseeb@gmail.com>
2020-10-17 23:28:08 -0400

    remove all auth, user and db related code

--- src/app.js
+++ src/app.js
@@ -8,8 +8,5 @@ const server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
 
-const UserController = require('./routes/UserController');
-app.use('/api/user', UserController);
-
 const KinController = require('./routes/KinController');
 app.use('/api/kin', KinController);
